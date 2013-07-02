f184403a46f9ef72a1ce5e0f9b836a176bd4c938
Tim Schmidt <tim.schmidt@dowjones.com>
2013-07-02 17:15:34 -0500

    Adding assembly attributes

--- src/ReSharper.Mustache.Plugin/Properties/AssemblyInfo.cs
+++ src/ReSharper.Mustache.Plugin/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using JetBrains.Application.PluginSupport;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +35,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: PluginTitle("ReSharper Mustache support")]
+[assembly: PluginDescription("Allow ReSharper to locate Mustache views")]
+[assembly: PluginVendor("Tim Schmidt")]
