1b9c00f120f80bb0e9456051604b8d5b0a1079c2
Tim Schmidt <tim.schmidt@dowjones.com>
2013-07-02 13:09:49 -0500

    First checkin

---
+++ MvcViewMustacheResolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.Asp.CustomReferences;
+
+namespace ReSharper.Mustache.Plugin
+{
+    public class MvcViewMustacheResolver : IMvcViewResolver
+    {
+        public bool IsApplicable(IProject project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<MvcViewLocationType, ICollection<string>> Values { get; private set; }
+    }
+}
