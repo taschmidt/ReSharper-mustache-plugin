ec2e6a698d73ce7fbaba0d21d331191746dbceef
Tim Schmidt <tim.schmidt@dowjones.com>
2013-07-02 13:34:24 -0500

    Implementing

--- src/ReSharper.Mustache.Plugin/MvcViewMustacheResolver.cs
+++ src/ReSharper.Mustache.Plugin/MvcViewMustacheResolver.cs
@@ -1,21 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using JetBrains.Metadata.Utils;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.Asp.CustomReferences;
+using JetBrains.ReSharper.Psi.Web.Util;
+using JetBrains.Util;
 
 namespace ReSharper.Mustache.Plugin
 {
     public class MvcViewMustacheResolver : IMvcViewResolver
     {
-        static MvcViewMustacheResolver()
-        {
-            
-        }
+        private static readonly AssemblyNameInfo NustacheAssembly = new AssemblyNameInfo("Nustache.Core");
+
+        private static readonly Dictionary<MvcViewLocationType, ICollection<string>> DefaultRoutes =
+            new Dictionary<MvcViewLocationType, ICollection<string>>
+                {
+                    {MvcViewLocationType.Unknown, EmptyList<string>.InstanceList},
+                    {MvcViewLocationType.AreaMaster, new[] {@"~\Areas\{2}\Views\{1}\{0}.mustache", @"~\Areas\{2}\Views\Shared\{0}.mustache"}},
+                    {MvcViewLocationType.AreaPartialView, new[] {@"~\Areas\{2}\Views\{1}\{0}.mustache", @"~\Areas\{2}\Views\Shared\{0}.mustache"}},
+                    {MvcViewLocationType.AreaView, new[] {@"~\Areas\{2}\Views\{1}\{0}.mustache", @"~\Areas\{2}\Views\Shared\{0}.mustache"}},
+                    {MvcViewLocationType.Master, new[] {@"~\Views\{1}\{0}.mustache", @"~\Views\Shared\{0}.mustache"}},
+                    {MvcViewLocationType.PartialView, new[] {@"~\Views\{1}\{0}.mustache", @"~\Views\Shared\{0}.mustache"}},
+                    {MvcViewLocationType.View, new[] {@"~\Views\{1}\{0}.mustache", @"~\Views\Shared\{0}.mustache"}}
+                };
+
         public bool IsApplicable(IProject project)
         {
-            throw new NotImplementedException();
+            AssemblyNameInfo assemblyNameInfo;
+            return ReferencedAssembliesService.IsProjectReferencingAssemblyByName(project, NustacheAssembly, out assemblyNameInfo);
         }
 
-        public IDictionary<MvcViewLocationType, ICollection<string>> Values { get; private set; }
+        public IDictionary<MvcViewLocationType, ICollection<string>> Values
+        {
+            get { return DefaultRoutes; }
+        }
     }
 }
